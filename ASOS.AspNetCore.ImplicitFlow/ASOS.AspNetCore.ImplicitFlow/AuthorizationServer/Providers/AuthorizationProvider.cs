73dfb8e66597579229d797a6a2025448e18c1435
Kannan Ramachandran <kannanpala@gmail.com>
2020-01-03 06:37:26 +0530

    ImplicitFlow

---
+++ ASOS.AspNetCore.ImplicitFlow/ASOS.AspNetCore.ImplicitFlow/AuthorizationServer/Providers/AuthorizationProvider.cs
@@ -0,0 +1,11 @@
+﻿using AspNet.Security.OpenIdConnect.Primitives;
+using AspNet.Security.OpenIdConnect.Server;
+using System;
+using System.Threading.Tasks;
+
+namespace AuthorizationServer.Providers
+{
+    public class AuthorizationProvider : OpenIdConnectServerProvider
+    {
+    }
+}
